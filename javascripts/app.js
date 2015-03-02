446bca407e307b208c5c4ce127c89107aeb0d6d7
jmovius <jmovius@J-Laptop.local>
2015-03-01 18:28:56 -0800

    Updating HTML, Adding JavaScript
    Updating all HTML tabs to correct width. Adding JavaScript app.js file
    (up to “Refactoring the code using a function” in the book).

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,4 +1,26 @@
 var main = function () {
 	"use strict;"
+
+	var makeTabActive = function (tabNumber) {
+		var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
+		$(".tabs span").removeClass("active"); // Make all tabs inactive.
+		$(tabSelector).addClass("active"); // Make the first tab active.
+		$("main .content").empty(); // Empty the main content so we can recreate it.
+	}
+
+	$(".tabs a:nth-child(1)").on("click", function () {
+		makeTabActive(1);
+		return false; // Return false, so we don't follow the link.
+	});
+
+	$(".tabs a:nth-child(2)").on("click", function () {
+		makeTabActive(2);
+		return false;
+	});
+
+	$(".tabs a:nth-child(3)".on("click", function () {
+		makeTabActive(3);
+		return false;
+	});
 };
 $(document).ready(main);
