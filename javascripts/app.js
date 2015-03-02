b162068619c3273242218ce87877d9959acf7b5d
jmovius <jmovius@J-Laptop.local>
2015-03-01 18:51:26 -0800

    Refactored JavaScript to use forEach
    JavaScript will now use a forEach loop to create a click element for
    all existing tabs (this code is scalable to any number of tabs).

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,26 +1,15 @@
 var main = function () {
 	"use strict;"
 
-	var makeTabActive = function (tabNumber) {
-		var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
-		$(".tabs span").removeClass("active"); // Make all tabs inactive.
-		$(tabSelector).addClass("active"); // Make the first tab active.
-		$("main .content").empty(); // Empty the main content so we can recreate it.
-	}
-
-	$(".tabs a:nth-child(1)").on("click", function () {
-		makeTabActive(1);
-		return false; // Return false, so we don't follow the link.
-	});
-
-	$(".tabs a:nth-child(2)").on("click", function () {
-		makeTabActive(2);
-		return false;
-	});
-
-	$(".tabs a:nth-child(3)").on("click", function () {
-		makeTabActive(3);
-		return false;
+	// For each tab that exists.
+	$(".tabs a span").toArray().forEach(function (element) {
+		// Create a click handler.
+		$(element).on("click", function () {
+			$(".tabs a span").removeClass("active"); // Remove active class.
+			$(element).addClass("active"); // Add active class to active tab.
+			$("main .content").empty(); // Remove content from main.
+			return false; // Return false to avoid following link.
+		});
 	});
 };
 $(document).ready(main);
