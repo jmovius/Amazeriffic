7e7fa6c70c7304633b7d037bb538d7d20c033fc9
jmovius <jmovius@J-Laptop.local>
2015-05-02 14:43:31 -0700

    Removing Jaket Directory
    Jaket directory is no longer needed. Simplifying project.

--- VagrantVM/app/Jaket/public/javascripts/app.js
+++
@@ -1,8 +0,0 @@
-// Client-side code
-/* jshint browser: true, jquery: true, curly: true, eqeqeq: true, forin: true, immed: true, indent: 4, latedef: true, newcap: true, nonew: true, quotmark: double, strict: true, undef: true, unused: true */
-var main = function () {
-	"use strict";
-
-	// Do stuff...
-};
-$(document).ready(main);
