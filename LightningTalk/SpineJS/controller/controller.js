83073637bc34f30e18583c4da765f0b9e5518263
jmovius <jmovius@MacBook-Pro.local>
2015-05-14 18:10:39 -0700

    Finalized Code
    Push includes previous code that separated the model and controller
    logic from each other.
    
    Moved logic around to better conform to MVC pattern.
    
    Added a button to the view to allow users to delete all comments.

--- LightningTalk/SpineJS/controller/controller.js
+++ LightningTalk/SpineJS/controller/controller.js
@@ -1,64 +1,25 @@
 (function (Spine, $, exports){
+    "use strict";
+
+	var _hiddenClass = "hidden"; // Set the name of the class used to hide comments.
+    var Comment = exports.Comment; // Retrieve model from the global namespace.
+
+    // Initialize JQuery Objects
     var $inputComment = $("#inputComment"),
         $submitComment = $("#submitComment"),
         $commentsList = $("#commentsList"),
-        $commentItem = $("div.commentSpan");
-    
-    /* Model */
-    var Comment = Spine.Model.sub(); // Init new Spine model object by extending Spine.Model.
-    Comment.configure("Comment", "comment", "isHidden"); // Object name and private members
-    Comment.extend(Spine.Model.Local); // Set Comment to store to local data.
+        $deleteComments = $("#deleteComments");
 
-    // Member functions.
-    Comment.include({
-        buildComment: function (i) {
-            var val = "",
-                hidden = "";
-
-            //console.log(this.id);
-
-            if(this.isHidden) {
-                hidden = " hidden";
-            }
-
-            val = "<div id=\"c-" + i + "\" class=\"visible" + hidden + "\">" + this.comment + "</div>";
-            return($("<li>").html(val).attr("class", "commentListItem"));
-        },
-        toggleHidden: function () {
-            $("#" + this.id).toggleClass("hidden", !this.isHidden);
-            this.isHidden = (!this.isHidden);
-            this.save();
-        }
-    });
-
-    Comment.bind("create", function (cmt) {
-        $inputComment.val("");
-        $commentsList.append(cmt.buildComment(Comment.count()-1));
-    });
-    
-    // Pull local data and load it to Comment.
-    Comment.fetch();
-//    Comment.destroyAll(); // Uncomment to destroy all comments.
-
-    var initComments = function () {
-        var cmt = Comment.all(),
-            len = cmt.length,
-            output = [],
-            i, hide;
-
-        cmt[0].buildComment();
-
-        $commentsList.empty();
-        for(i = 0; i < len; i++) {
-            output.push(cmt[i].buildComment(i));
-        }
-        $commentsList.append(output);
-    };
-
+    // Function will create a comment, then append it to the view.
     var submitComment = function (msg) {
-        if(msg !== "") {
-            var cmt = new Comment({comment:msg, isHidden:false});
-            cmt.save();
+    	var val, cmt;
+
+      	if(msg !== "") {
+            cmt = Comment.getNewComment(msg); // Get new comment object.
+      		val = cmt.getJqueryWithTag("li", _hiddenClass); // Get the JQuery object and store it to val.
+
+            $inputComment.val("");
+            $commentsList.append(val);
         }
     };
 
@@ -68,6 +29,7 @@
     });
     CommentController.extend(Spine.Events);
 
+    // Submit a comment using the + button.
     var buttonController = new CommentController({
         el: $submitComment,
         events: {
@@ -78,6 +40,7 @@
         }
     });
 
+    // Submit a comment by hitting enter from the input box.
     var inputController = new CommentController({
         el: $inputComment,
         events: {
@@ -90,18 +53,35 @@
         }
     });
 
-    initComments();
-
+    // Click a comment adds the CSS class "hidden".
     var hiddenController = new CommentController({
-        el: $("ul"),
+        el: $commentsList,
         events: {
             "click": "click"
         },
         click: function (e) {
+        	// Acquire the comment based on the ID of the item clicked.
             var cmt = Comment.find(e.target.id);
-            console.log(cmt.comment);
-            cmt.toggleHidden();
+
+            // Update the DOM to toggle the hiddenClass attribute.
+            $("#" + cmt.id).toggleClass(_hiddenClass, cmt.toggleHiddenClass());
         }
     });
+
+    // Click the "Delete All Comments" button destroys all objects stored in the Comments data model.
+    var buttonDeleteController = new CommentController({
+        el: $deleteComments,
+        events: {
+            "click": "click"
+        },
+        click: function () {
+            Comment.destroyAll(); // Destroy all comments.
+            $commentsList.empty(); // Empty comments from the view.
+        }
+    });
+
+    // Initialize all comments.
+    $commentsList.empty();
+    $commentsList.append(Comment.initCommentsWithTag("li", _hiddenClass));
 
 })(Spine, Spine.$, window);
